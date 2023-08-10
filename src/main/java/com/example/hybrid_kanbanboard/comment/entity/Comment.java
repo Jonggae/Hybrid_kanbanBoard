5aca58a72640459a30b5c636ce18683d4b4103cf
hee0hee <132984807+hee0hee@users.noreply.github.com>
2023-08-10 14:48:38 +0900

    [Update] 동환님 수정

--- src/main/java/com/example/hybrid_kanbanboard/comment/entity/Comment.java
+++ src/main/java/com/example/hybrid_kanbanboard/comment/entity/Comment.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Comment extends TimeStamped {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
