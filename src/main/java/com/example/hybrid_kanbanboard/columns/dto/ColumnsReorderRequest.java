ef1995d5c21d022a906326d134494734fe943cfd
nm0628 <dlaskaaud@naver.com>
2023-08-11 18:27:06 +0900

    [Update] 컬럼 이동 기능 구현

---
+++ src/main/java/com/example/hybrid_kanbanboard/columns/dto/ColumnsReorderRequest.java
@@ -0,0 +1,8 @@
+package com.example.hybrid_kanbanboard.columns.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ColumnsReorderRequest {
+    Long columnPosition;
+}
--- src/main/java/com/example/hybrid_kanbanboard/columns/repository/ColumnsRepository.java
+++ src/main/java/com/example/hybrid_kanbanboard/columns/repository/ColumnsRepository.java
@@ -1,8 +1,23 @@
 package com.example.hybrid_kanbanboard.columns.repository;
 
 import com.example.hybrid_kanbanboard.columns.entity.Columns;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ColumnsRepository extends JpaRepository<Columns, Long> {
+    @Modifying
+    @Query("UPDATE Columns SET columnPosition = columnPosition + 1 WHERE columnPosition >= :newPosition AND columnPosition < :oldPosition")
+    void incrementBelowToPosition(@Param("newPosition") Long newPosition,
+                                  @Param("oldPosition") Long oldPosition);
 
+    @Modifying
+    @Query("UPDATE Columns SET columnPosition = columnPosition - 1 WHERE columnPosition <= :newPosition AND columnPosition > :oldPosition ")
+    void decrementAboveToPosition(@Param("newPosition") Long newPosition,
+                                  @Param("oldPosition") Long oldPosition);
+
+    @Modifying
+    @Query("UPDATE Columns SET columnPosition = columnPosition - 1 WHERE columnPosition >= :position")
+    void decrementBelow(@Param("position") Long position);
 }
