0c30d5f6dc94e2cbe59895e32a1ba3784ca20f5d
alxqkekrroqkfwk <fexxet@icloud.com>
2023-08-14 10:58:25 +0900

    [Refactor] 연관관계 설정

--- src/main/java/com/example/hybrid_kanbanboard/columns/dto/ColumnsResponseDto.java
+++ src/main/java/com/example/hybrid_kanbanboard/columns/dto/ColumnsResponseDto.java
@@ -1,8 +1,12 @@
 package com.example.hybrid_kanbanboard.columns.dto;
 
+import com.example.hybrid_kanbanboard.card.dto.CardResponseDto;
 import com.example.hybrid_kanbanboard.columns.entity.Columns;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -12,12 +16,15 @@ public class ColumnsResponseDto {
     private Long ColumnId;
     private String columnName;
     private String columnMaker;
+    private List<CardResponseDto> cardResponseDtos;
 
     public ColumnsResponseDto(Columns column) {
         this.ColumnId = column.getColumnId();
         this.BoardId = column.getBoard().getBoardId();
         this.columnMaker = column.getUser().getUserName();
         this.columnName = column.getColumnName();
+        this.cardResponseDtos = column.getCards()
+                .stream().map(CardResponseDto::new).collect(Collectors.toList());
     }
 
 
